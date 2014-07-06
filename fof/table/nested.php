6a7cc2f61288a099c60e5f6b80c4312708b6dc15
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2014-07-06 11:00:50 +0300

    Fixing Table\Nested strict notice

--- fof/table/nested.php
+++ fof/table/nested.php
@@ -13,6 +13,7 @@ use FOF30\String\Utils as FOFStringUtils;
 
 use Exception, RuntimeException, UnexpectedValueException;
 
+use FOF30\Utils\ArrayUtils\ArrayUtils;
 use JDatabaseDriver, JDatabaseQuery;
 
 // Protect from unauthorized access
@@ -201,9 +202,93 @@ class Nested extends Table
 	/**
 	 * Makes a copy of the record, inserting it as the last child of the current node's parent.
 	 *
-	 * @return self
+	 * The object is reset after the copy.
+	 *
+	 * @return bool True on success.
 	 */
-	public function copy()
+	public function copy($cid = null)
+	{
+		//We have to cast the id as array, or the helper function will return an empty set
+		if($cid)
+		{
+			$cid = (array) $cid;
+		}
+
+		ArrayUtils::toInteger($cid);
+		$k = $this->_tbl_key;
+
+		if (count($cid) < 1)
+		{
+			if ($this->$k)
+			{
+				$cid = array($this->$k);
+			}
+			else
+			{
+				$this->setError("No items selected.");
+
+				return false;
+			}
+		}
+
+		$created_by  = $this->getColumnAlias('created_by');
+		$created_on  = $this->getColumnAlias('created_on');
+		$modified_by = $this->getColumnAlias('modified_by');
+		$modified_on = $this->getColumnAlias('modified_on');
+
+		$locked_byName = $this->getColumnAlias('locked_by');
+		$checkin       = in_array($locked_byName, $this->getKnownFields());
+
+		foreach ($cid as $item)
+		{
+			// Prevent load with id = 0
+
+			if (!$item)
+			{
+				continue;
+			}
+
+			$this->load($item);
+
+			if ($checkin)
+			{
+				// We're using the checkin and the record is used by someone else
+
+				if ($this->isCheckedOut($item))
+				{
+					continue;
+				}
+			}
+
+			if (!$this->onBeforeCopy($item))
+			{
+				continue;
+			}
+
+			$this->$k           = null;
+			$this->$created_by  = null;
+			$this->$created_on  = null;
+			$this->$modified_on = null;
+			$this->$modified_by = null;
+
+			// Let's fire the event only if everything is ok
+			if ($this->create($this->getData()))
+			{
+				$this->onAfterCopy($item);
+			}
+
+			$this->reset();
+		}
+
+		return true;
+	}
+
+	/**
+	 * Creates and returns a duplicate copy of the node
+	 *
+	 * @return static
+	 */
+	public function duplicate()
 	{
 		return $this->create($this->getData());
 	}
